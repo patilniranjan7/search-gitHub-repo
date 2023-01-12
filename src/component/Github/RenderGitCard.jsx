8750a9227b48e3c093d7e913ff94ea39485c6f7e
niranjan patil <patilniranjan7878@gmail.com>
2023-01-12 22:38:57 +0530

    Feat: Added card view

---
+++ src/component/Github/RenderGitCard.jsx
@@ -0,0 +1,19 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+import CardLayout from '../../layout/CardLayout'
+import CardUI from '../UI/CardUI'
+import GitCard from './GitCard'
+
+export default function RenderGitCard({ data, ...props }) {
+    return (
+        <Grid container>
+            {
+                (data || []).map((gitdata) =>
+                    <CardUI>
+                        <GitCard gitdata={gitdata} />
+                    </CardUI>
+                )
+            }
+        </Grid>
+    )
+}
